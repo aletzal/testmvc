ec3ffdba5ff9200f3fc18d52aa5e68217475f94c
Alexey <aletzal@gmail.com>
2017-12-12 00:15:25 +0300

    Added user friendly message for NumberFormatException in ticketForm

---
+++ src/main/java/com/example/repository/FilmRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+/**
+ * Created by Admin on 11.12.17.
+ */
+public class FilmRepository {
+}
---
+++ src/main/java/com/example/service/FilmService.java
@@ -0,0 +1,7 @@
+package com.example.service;
+
+/**
+ * Created by Admin on 11.12.17.
+ */
+public class FilmService {
+}
